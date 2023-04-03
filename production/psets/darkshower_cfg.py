01ea71ae31e030ce32fcde531dde9d36593645ba
isuarez <isuarez@cern.ch>
2023-04-03 07:08:50 -0700

    changing the outputfile name

--- production/psets/darkshower_cfg.py
+++ production/psets/darkshower_cfg.py
@@ -83,7 +83,7 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
         filterName = cms.untracked.string('')
     ),
     eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
-    fileName = cms.untracked.string('file:output.root'),
+    fileName = cms.untracked.string('file:output_gensim.root'),
     outputCommands = process.RAWSIMEventContent.outputCommands,
     splitLevel = cms.untracked.int32(0)
 )
