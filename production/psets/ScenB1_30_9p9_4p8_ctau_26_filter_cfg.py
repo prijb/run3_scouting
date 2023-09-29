2f5bee0761e3660e24db7b5eef4c1d117307f71c
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2023-09-29 14:20:09 -0700

    WIP multi-submit of filtered samples

---
+++ production/psets/ScenB1_30_9p9_4p8_ctau_26_filter_cfg.py
@@ -0,0 +1,246 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/scenB1_30_9p9_4p8_ctau_26_filter.py --python_filename darkshower_B1_cfg.py --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:output_gensim.root --conditions 124X_mcRun3_2022_realistic_v12 --beamspot Realistic25ns13p6TeVEarly2022Collision --customise_commands process.source.numberEventsInLuminosityBlock=cms.untracked.uint32(100) --step GEN,SIM --geometry DB:Extended --era Run3 --no_exec --mc -n 5
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_cff import Run3
+
+process = cms.Process('SIM',Run3)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic25ns13p6TeVEarly2022Collision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(5),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Configuration/GenProduction/python/scenB1_30_9p9_4p8_ctau_26_filter.py nevts:5'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(1),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
+    fileName = cms.untracked.string('file:output_gensim.root'),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+if hasattr(process, "XMLFromDBSource"): process.XMLFromDBSource.label="Extended"
+if hasattr(process, "DDDetectorESProducerFromDB"): process.DDDetectorESProducerFromDB.label="Extended"
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '124X_mcRun3_2022_realistic_v12', '')
+
+process.MuFilter = cms.EDFilter("PythiaFilter",
+    MaxEta = cms.untracked.double(10),
+    MinEta = cms.untracked.double(-10),
+    MinPt = cms.untracked.double(2),
+    MotherID = cms.untracked.int32(999999),
+    ParticleID = cms.untracked.int32(13),
+    Status = cms.untracked.int32(0)
+)
+
+
+process.generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CP5Settings',
+            'processParameters'
+        ),
+        processParameters = cms.vstring(
+            'HiggsSM:gg2H = on',
+            '25:m0 =125',
+            '25:addChannel = 1 0.5 102 4900101 -4900101',
+            '25:addChannel = 1 0.5 102 4900102 -4900102',
+            '25:0:onMode=0',
+            '25:1:onMode=0',
+            '25:2:onMode=0',
+            '25:3:onMode=0',
+            '25:4:onMode=0',
+            '25:5:onMode=0',
+            '25:6:onMode=0',
+            '25:7:onMode=0',
+            '25:8:onMode=0',
+            '25:9:onMode=0',
+            '25:10:onMode=0',
+            '25:11:onMode=0',
+            '25:12:onMode=0',
+            '25:13:onMode=0',
+            'HiddenValley:Ngauge = 3',
+            'HiddenValley:nFlav = 2',
+            'HiddenValley:fragment = on',
+            'HiddenValley:FSR = on',
+            'HiddenValley:alphaOrder = 1',
+            'HiddenValley:setLambda = on',
+            'HiddenValley:Lambda = 30',
+            'HiddenValley:pTminFSR = 33.0',
+            'HiddenValley:spinFv=0',
+            'HiddenValley:probVector=0.75',
+            'HiddenValley:separateFlav = on',
+            'HiddenValley:probKeepEta1 = 1.0',
+            '4900101:m0 = 31.551517243857766',
+            '4900102:m0 = 31.715482756142233',
+            '4900111:m0 = 9.9',
+            '4900211:m0 = 9.9',
+            '4900221:m0 = 30',
+            '4900113:m0 = 30',
+            '4900213:m0 = 30',
+            '4900113:addChannel = 1 1.00 91 4900211 -4900211',
+            '4900213:addChannel = 1 1.00 91 4900211 4900111',
+            '999999:all = GeneralResonance void 1 0 0 4.8 0.001 0. 0. 0.',
+            '999999:addChannel = 1 0.0541 91 1 -1',
+            '999999:addChannel = 1 0.217 91 2 -2',
+            '999999:addChannel = 1 0.0541 91 3 -3',
+            '999999:addChannel = 1 0.211 91 4 -4',
+            '999999:addChannel = 1 0.162 91 11 -11',
+            '999999:addChannel = 1 0.162 91 13 -13',
+            '999999:addChannel = 1 0.139 91 15 -15',
+            '999999:tau0 = 1.1e-05',
+            '4900221:addChannel = 1 0.5 91 4900211 4900211 4900111',
+            '4900221:addChannel = 1 0.5 91 -4900211 -4900211 4900111',
+            '4900221:tau0 = 0.0',
+            '4900111:addChannel = 1 1.0 91 999999 999999',
+            '4900111:tau0 = 26.099999999999998',
+            '4900211:onMode = 0'
+        ),
+        pythia8CP5Settings = cms.vstring(
+            'Tune:pp 14',
+            'Tune:ee 7',
+            'MultipartonInteractions:ecmPow=0.03344',
+            'MultipartonInteractions:bProfile=2',
+            'MultipartonInteractions:pT0Ref=1.41',
+            'MultipartonInteractions:coreRadius=0.7634',
+            'MultipartonInteractions:coreFraction=0.63',
+            'ColourReconnection:range=5.176',
+            'SigmaTotal:zeroAXB=off',
+            'SpaceShower:alphaSorder=2',
+            'SpaceShower:alphaSvalue=0.118',
+            'SigmaProcess:alphaSvalue=0.118',
+            'SigmaProcess:alphaSorder=2',
+            'MultipartonInteractions:alphaSvalue=0.118',
+            'MultipartonInteractions:alphaSorder=2',
+            'TimeShower:alphaSorder=2',
+            'TimeShower:alphaSvalue=0.118',
+            'SigmaTotal:mode = 0',
+            'SigmaTotal:sigmaEl = 22.08',
+            'SigmaTotal:sigmaTot = 101.037',
+            'PDF:pSet=LHAPDF6:NNPDF31_nnlo_as_0118'
+        ),
+        pythia8CommonSettings = cms.vstring(
+            'Tune:preferLHAPDF = 2',
+            'Main:timesAllowErrors = 10000',
+            'Check:epTolErr = 0.01',
+            'Beams:setProductionScalesFromLHEF = off',
+            'SLHA:minMassSM = 1000.',
+            'ParticleDecays:allowPhotonRadiation = on'
+        )
+    ),
+    comEnergy = cms.double(13600.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator+process.MuFilter)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path).insert(0, process.ProductionFilterSequence)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+
+
+# Customisation from command line
+
+process.source.numberEventsInLuminosityBlock=cms.untracked.uint32(100)
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
--- production/submitter.py
+++ production/submitter.py
@@ -25,51 +25,64 @@ if __name__ == '__main__':
     ]
     '''
 
-    reqname = 'DarkShower_ScenarioB1_default'  # Scouting For Scouting
+    #reqname = 'DarkShower_ScenarioB1_default'  # Scouting For Scouting
 
-    fragment = "psets/darkshower_cfg.py"  # This defines the physics
+    #fragment = "psets/darkshower_cfg.py"  # This defines the physics
+    #
+
+    requests = {
+        "ScenB1_30_9p9_4p8_ctau_26_filter": "psets/ScenB1_30_9p9_4p8_ctau_26_filter_cfg.py",
+        "ScenA_20_5p0_1p2_ctau_23_filter": "psets/ScenA_20_5p0_1p2_ctau_23_filter_cfg.py",
+    }
     events_per_point = 20
     events_per_job = 10
     njobs = int(events_per_point)//events_per_job
 
-    tag = "v4p0"  # v0p8 first one with compiling on worker, 14 switch to el8 from rhel8
+    tag = "v5p0"  # v0p8 first one with compiling on worker, 14 switch to el8 from rhel8
     # 17 - trying to switch of pythia multithreading
     campaign = "Run3Summer22GS"
 
-    task = CondorTask(
-        sample = DummySample(
-            dataset=f"/{reqname}/{campaign}_{tag}/AODSIM",
-            N=njobs,
-            nevents=int(events_per_point),
-        ),
-        output_name = "output.root",
-        executable = "executables/test_signal_production.sh",
-        tarfile = "package.tar.gz",  # contains precompiled CMSSW_12_4_12, pythia and RAWSIM/AODSIM configs
-        additional_input_files = [fragment],  # this is signal point dependent
-        scram_arch = "el8_amd64_gcc10",  # not directly used, but better define it
-        # should we switch to el8_amd64_gcc10??
-        cmssw_version = "CMSSW_12_4_12",  # not directly used, but better define it
-        open_dataset = False,
-        files_per_output = 1,
-        arguments = fragment,  # tell the job the name of the fragment
-        condor_submit_params = {
-            "sites":"T2_US_UCSD", #
-            "classads": [
-                ["param_nevents",events_per_job],
-                ["metis_extraargs",""],
-                ["JobBatchName",reqname],
-                ["SingularityImage", "/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel8-m-m20230223"],
-                #["SingularityImage", "/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmssw/cc7:x86_64-latest"],
-                #["SingularityImage", "/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmssw/el8:x86_64-d20230317"],
-                #["SingularityImage", "/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel7-m20221104"],
-                ],
-            "requirements_line": 'Requirements = (HAS_SINGULARITY=?=True)'  # && (HAS_CVMFS_cms_cern_ch =?= true) && {extra_requirements})'.format(extra_requirements=extra_requirements),
-            },
-        tag = tag,
-        min_completion_fraction = 0.90,
-        )
+    tasks = []
 
-    task.process()
-    total_summary[task.get_sample().get_datasetname()] = task.get_task_summary()
+    for reqname in requests:
+        fragment = requests[reqname]
+
+        task = CondorTask(
+            sample = DummySample(
+                dataset=f"/{reqname}/{campaign}_{tag}/AODSIM",
+                N=njobs,
+                nevents=int(events_per_point),
+            ),
+            output_name = "output.root",
+            executable = "executables/test_signal_production.sh",
+            tarfile = "package.tar.gz",  # contains precompiled CMSSW_12_4_12, pythia and RAWSIM/AODSIM configs
+            additional_input_files = [fragment],  # this is signal point dependent
+            scram_arch = "el8_amd64_gcc10",  # not directly used, but better define it
+            # should we switch to el8_amd64_gcc10??
+            cmssw_version = "CMSSW_12_4_12",  # not directly used, but better define it
+            open_dataset = False,
+            files_per_output = 1,
+            arguments = fragment,  # tell the job the name of the fragment
+            condor_submit_params = {
+                "sites":"T2_US_UCSD", #
+                "classads": [
+                    ["param_nevents",events_per_job],
+                    ["metis_extraargs",""],
+                    ["JobBatchName",reqname],
+                    ["SingularityImage", "/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel8-m-m20230223"],
+                    #["SingularityImage", "/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmssw/cc7:x86_64-latest"],
+                    #["SingularityImage", "/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmssw/el8:x86_64-d20230317"],
+                    #["SingularityImage", "/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel7-m20221104"],
+                    ],
+                "requirements_line": 'Requirements = (HAS_SINGULARITY=?=True)'  # && (HAS_CVMFS_cms_cern_ch =?= true) && {extra_requirements})'.format(extra_requirements=extra_requirements),
+                },
+            tag = tag,
+            min_completion_fraction = 0.90,
+            )
+        tasks.append(task)
+
+    for task in tasks:
+        task.process()
+        total_summary[task.get_sample().get_datasetname()] = task.get_task_summary()
 
     StatsParser(data=total_summary, webdir="~/public_html/dump/scouting/").do()
