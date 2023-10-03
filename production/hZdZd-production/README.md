fd4266fac5cadb7f2650284b8ca787b7db08695a
fernance <celia.fernandez.madrazo@cern.ch>
2023-10-03 09:50:23 -0700

    Add card templates for hZdZd gridpack production

---
+++ production/hZdZd-production/README.md
@@ -0,0 +1,42 @@
+# HAHM production
+
+The production takes as an example the existing HAHM hToZdZd simulation for Run 3 with mZd = 20 GeV and epsilon = 1e-8. Central fragment for this sample can be accessed through:
+
+```
+https://cms-pdmv.cern.ch/mcm/public/restapi/requests/get_fragment/EXO-Run3Summer22EEwmLHEGS-01017
+```
+
+Additional info:
+McM record: https://cms-pdmv.cern.ch/mcm/requests?page=0&prepid=EXO-Run3Summer22EEwmLHEGS-01017
+
+
+## Gridpack production
+
+Template cards to make the gridpacks are found in ```card-templates/``` and are made from ones extracted from the central gridpack:
+```
+/cvmfs/cms.cern.ch/phys_generator/gridpacks/RunIII/13p6TeV/slc7_amd64_gcc10/madgraph/LL_HAHM_MS_400/LL_HAHM_MS_400_kappa_0p01_MZd_20_eps_1e-08_slc7_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz
+```
+
+To create the new cards for a predefined set of (mZd, epsilon) values fist add the chosen points in ```makeCards.py``` with the following structure:
+```
+model_grid = {}
+model_grid["5.0"] = ['1e-8', '2e-7']
+```
+and then run:
+```
+python3 makeCards.py
+``` 
+The output cards will be available in the ```hZdZd/``` dir.
+
+To create the gridpacks the central generation repository should be cloned:
+```
+git clone https://github.com/cms-sw/genproductions.git -b mg29x
+```
+The new cards should be copied within the ```genproductions/bin/MadGraph5_aMCatNLO/cards``` folder. Then, in ```genproductions/bin/MadGraph5_aMCatNLO/``` the gridpacks are created by running:
+```
+./gridpack_generation.sh LL_HAHM_MS_400_kappa_0p01_MZd_5p0_eps_1e-8 cards/hZdZd/hZdZd_mZd_5p0_eps_1e-8/
+```
+
+## Fragment production
+
+(in progress)
---
+++ production/hZdZd-production/makeCards.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+inputdir = "card-templates/"
+tmp_customizecards = "LL_HAHM_MS_400_kappa_0p01_MZd_ZDMASS_eps_EPSILON_customizecards.dat"
+tmp_extramodels    = "LL_HAHM_MS_400_kappa_0p01_MZd_ZDMASS_eps_EPSILON_extramodels.dat"
+tmp_proc_card      = "LL_HAHM_MS_400_kappa_0p01_MZd_ZDMASS_eps_EPSILON_proc_card.dat"
+tmp_run_card       = "LL_HAHM_MS_400_kappa_0p01_MZd_ZDMASS_eps_EPSILON_run_card.dat"
+
+with open(inputdir + tmp_customizecards, 'r') as file_:
+    txt_customizecards = file_.read()
+
+with open(inputdir + tmp_extramodels, 'r') as file_:
+    txt_extramodels = file_.read()
+
+with open(inputdir + tmp_proc_card, 'r') as file_:
+    txt_proc_card = file_.read()
+
+with open(inputdir + tmp_run_card, 'r') as file_:
+    txt_run_card = file_.read()
+
+## Define the model grid: mass ; [epsilon]
+model_grid = {}
+model_grid["5.0"] = ['1e-8', '2e-7']
+
+for mass in model_grid.keys():
+    for epsilon in model_grid[mass]:
+        outdir = 'hZdZd/hZdZd_mZd_{ZDMASS}_eps_{EPSILON}/'.format(ZDMASS = mass.replace('.', 'p'), EPSILON = epsilon)
+        if not os.path.isdir(outdir):
+            os.makedirs(outdir)
+        customizecards = tmp_customizecards.replace('ZDMASS', '{ZDMASS}').replace('EPSILON', '{EPSILON}').format(ZDMASS = mass.replace('.', 'p'), EPSILON = epsilon)
+        with open(outdir + customizecards, 'w') as file_:
+            out_customizecards = txt_customizecards.format(ZDMASS = mass, EPSILON = epsilon) # not replace the '.'
+            file_.write(out_customizecards)
+            file_.close()
+        extramodels = tmp_extramodels.replace('ZDMASS', '{ZDMASS}').replace('EPSILON', '{EPSILON}').format(ZDMASS = mass.replace('.', 'p'), EPSILON = epsilon)
+        with open(outdir + extramodels, 'w') as file_:
+            out_extramodels = txt_extramodels # No need to format (?)
+            file_.write(out_extramodels)
+            file_.close()
+        proc_card = tmp_proc_card.replace('ZDMASS', '{ZDMASS}').replace('EPSILON', '{EPSILON}').format(ZDMASS = mass.replace('.', 'p'), EPSILON = epsilon)
+        with open(outdir + proc_card, 'w') as file_:
+            out_proc_card = txt_proc_card.format(ZDMASS = mass.replace('.', 'p'), EPSILON = epsilon)
+            file_.write(out_proc_card)
+            file_.close()
+        run_card = tmp_run_card.replace('ZDMASS', '{ZDMASS}').replace('EPSILON', '{EPSILON}').format(ZDMASS = mass.replace('.', 'p'), EPSILON = epsilon)
+        with open(outdir + run_card, 'w') as file_:
+            out_run_card = txt_run_card # No need to format (?)
+            file_.write(out_run_card)
+            file_.close()
+
+
+
