439ac4f3f6fd2d4114e44954a5e33c70d2a72f2a
fernance <celia.fernandez.madrazo@cern.ch>
2023-10-09 09:38:51 -0700

    Add script to create the cfg files with central configuration

--- production/hZdZd-production/makeCfgs.sh
+++ production/hZdZd-production/makeCfgs.sh
@@ -2,9 +2,11 @@
 
 export X509_USER_PROXY=$(voms-proxy-info -path)
 export SCRAM_ARCH=el8_amd64_gcc10
-export FRAGMENTS=$1
+FRAGMENTS=$1
 WORK=$PWD
+CFGPATH="$PWD/configs"
 
+# Set CMSSW ENV and cp fragments
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 if [ -r CMSSW_12_4_11_patch3/src ] ; then
   echo release CMSSW_12_4_11_patch3 already exists
@@ -20,14 +22,16 @@ cd CMSSW_12_4_11_patch3/src
 scram b
 cmsenv
 
+# Create the cfg files
+if [ -r $CFGPATH ] ; then
+  echo "configs/ dir already exists"
+else
+  mkdir $CFGPATH
+fi
 EVENTS=10
 SEED=$(($(date +%s) % 100 + 1));
-
-for FILE in Configuration/GenProduction/python/*
-do
-    echo $FILE;
+for FILE in Configuration/GenProduction/python/*.py; do
     if [ $FILE == "Configuration/GenProduction/python/__init__.py" ]; then
-	echo "Continueeee"
         continue;
     fi
     prefix=${FILE%.*};
@@ -35,5 +39,6 @@ do
     CFG="$prefix$suffix"; 
     ROOT="file:$prefix.root"
     cmsDriver.py $FILE --python_filename $CFG --eventcontent RAWSIM,LHE --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM,LHE --fileout ROOT --conditions 124X_mcRun3_2022_realistic_postEE_v1 --beamspot Realistic25ns13p6TeVEarly2022Collision --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${SEED})" --step LHE,GEN,SIM --geometry DB:Extended --era Run3 --no_exec --mc -n $EVENTS;
-    mv $FILE $WORK
+    mv $FILE $CFGPATH;
 done
+
