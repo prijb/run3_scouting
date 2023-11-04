d88234065a94fbfba59d14a3621f746abd118b4a
leonardo <leonardo.giannini@cern.ch>
2023-11-03 17:49:44 -0700

    add 2023 to sh submission files and remove the duplicates

--- condor/runScoutingLooper_onCondor.sh
+++ condor/runScoutingLooper_onCondor.sh
@@ -6,10 +6,10 @@ usage()
 {
     echo "Usage:"
     echo ""
-    echo "  sh condor/runScoutingLooper_onCondor.sh ['notar'] input_dir"
+    echo "  sh condor/runScoutingLooper_onCondor.sh ['notar'] ['2023'] input_dir"
     echo ""
     echo "The output directory will be created in /ceph/cms/store/user/$USER/Run3ScoutingOutput/"
-    echo "Control the jobs to be run by editing runScoutingLooper_onCondor.sub"
+    echo "Control the jobs to be run by editing runScoutingLooper_onCondor.sub or the corresponding 2023 file"
     echo ""
     exit
 }
@@ -17,17 +17,31 @@ usage()
 if [ -z $1 ]; then usage; fi
 
 notar=0
+year23=0
 indir=""
 
 if [ $# -gt 1 ]
 then
-    if [ "$1" == "notar" ]
-    then
-	notar=1
-	indir=$2
-    else
-	indir=$1
-    fi
+   if [ "$1" == "notar" ]
+   then
+      if [ "$2" == "2023" ]
+      then
+         notar=1
+         year23=1
+         indir=$3
+      else
+         notar=1
+         indir=$2
+      fi
+   else
+      if [ "$1" == "2023" ]
+      then
+         year23=1
+         indir=$2
+      else
+         indir=$1
+      fi
+   fi
 fi
 
 export SCOUTINGOUTPUTDIR=${indir}
@@ -38,8 +52,18 @@ mkdir -p /ceph/cms/store/user/$USER/Run3ScoutingOutput/$SCOUTINGOUTPUTDIR
 
 if [ ${notar} -gt 0 ]
 then
-    condor_submit condor/runScoutingLooper_onCondor.sub
+   if [ ${year23} -gt 0 ]
+   then
+      echo condor_submit condor/runScoutingLooper_onCondor2023.sub
+   else
+      echo condor_submit condor/runScoutingLooper_onCondor.sub
+   fi
 else
-    sh condor/create_package.sh
-    condor_submit condor/runScoutingLooper_onCondor.sub    
+   echo sh condor/create_package.sh
+   if [ ${year23} -gt 0 ]
+   then
+      echo condor_submit condor/runScoutingLooper_onCondor2023.sub
+   else
+      echo condor_submit condor/runScoutingLooper_onCondor.sub
+   fi
 fi
