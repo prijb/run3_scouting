050e1dee8aa543f42c76acc590b9ae6822e1edbe
fernance <celia.fernandez.madrazo@cern.ch>
2024-10-08 07:18:08 -0700

    Update condor scripts to be used with combine

--- condor/limits/create_package.sh
+++ condor/limits/create_package.sh
@@ -2,21 +2,28 @@
 
 #INDIR="datacards_all_Feb-21-2024"
 INDIR=$1
-PERIOD=$2
 
 # Start from ScoutingRun3/.
 rm tmp_create_package/ -rf
 mkdir -p tmp_create_package
 cd tmp_create_package
 mkdir -p ScoutingRun3
+cd ..
 
-cp ../HiggsAnalysis/ ScoutingRun3/. -r # Copy relevant folders
+cp HiggsAnalysis/ tmp_create_package/ScoutingRun3/. -r # Copy relevant folders
 #cp CMSSW_13_3_0 tmp_create_package/ScoutingRun3/. -r # Copy relevant folders
 #cp combineScripts tmp_create_package/ScoutingRun3/. -r 
-#tar -cf - ../combineScripts ../${INDIR} ../fitResults_${PERIOD} | tar -xf - -C ScoutingRun3/. # Copy cpp folder without the plot folders
 echo "Checking what is inside the package:"
-ls -la ScoutingRun3/
-tar -cf - ../combineScripts ../${INDIR} | tar -xf - -C ScoutingRun3/. # Copy cpp folder without the plot folders
+ls tmp_create_package/ScoutingRun3/
+for INDIR in "$@"; do
+    echo "Adding ${INDIR} to package.tar.gz..."
+    tar -cf - ${INDIR} | tar -xf - -C tmp_create_package/ScoutingRun3/. # Copy cpp folder without the plot folders
+    ls tmp_create_package/ScoutingRun3/
+done
+echo "Adding final combine scripts..."
+cd tmp_create_package
+tar -cf - ../combineScripts | tar -xf - -C ScoutingRun3/. # Copy cpp folder without the plot folders
+ls ScoutingRun3/
 
 tar -chJf package.tar.gz ScoutingRun3
 
