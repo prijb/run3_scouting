5c55954e6d7c39b881d684e90ecb98de4cfde737
fernance <celia.fernandez.madrazo@cern.ch>
2025-04-08 02:21:40 -0700

    Correct merging of datacards to not combine intermediate cards

--- combineScripts/combineDatacards.py
+++ combineScripts/combineDatacards.py
@@ -24,7 +24,7 @@ systematics.append(['mcstat_ch','stat22_ch','stat23_ch'])
 
 # Loop in cards of dir 1, look for the equivalent in dir 2 and merge:
 for card1 in os.listdir(inDir1):
-    if '.txt' not in card1 or 'combine' not in card1:
+    if '.txt' not in card1 or 'combine' not in card1 or 'tomerge' in card1:
         continue
     card_name1 = ''
     for portion in card1.split('_')[:-1]:
@@ -46,10 +46,11 @@ for card1 in os.listdir(inDir1):
             os.system("sed -i 's/CMS_eff_trg_2022/CMS_eff_trg     /g' %s/%s"%(inDir1, card1))
             os.system("sed -i 's/CMS_eff_trg_2023/CMS_eff_trg     /g' %s/%s"%(inDir2, card2))
             # Uncorrelate systematics
-            os.system("sed -i 's/mcstat_ch/stat22_ch/g' %s/%s"%(inDir1, card1))
-            os.system("sed -i 's/mcstat_ch/stat23_ch/g' %s/%s"%(inDir2, card2))
+            os.system("sed -i 's/mcstat_ch/stat22_ch/g' %s/%s"%(inDir1, tomerge_card1))
+            os.system("sed -i 's/mcstat_ch/stat23_ch/g' %s/%s"%(inDir2, tomerge_card2))
             print("COMBINING ", card_name1)
-            print("combineCards.py -S %s/%s %s/%s > %s/%sallEras.txt "%(inDir1, card1, inDir2, card2, outDir, card_name1))
+            #print("combineCards.py -S %s/%s %s/%s > %s/%sallEras.txt "%(inDir1, card1, inDir2, card2, outDir, card_name1))
+            print("combineCards.py -S %s/%s %s/%s > %s/%sallEras.txt "%(inDir1, tomerge_card1, inDir2, tomerge_card2, outDir, card_name1))
             os.system("combineCards.py -S %s/%s %s/%s > %s/%sallEras.txt "%(inDir1, tomerge_card1, inDir2, tomerge_card2, outDir, card_name1))
             os.system("text2workspace.py %s/%sallEras.txt"%(outDir, card_name1))
             break
