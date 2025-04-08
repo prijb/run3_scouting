05d513d11d993e196dbd812d9fb522217f79de0b
fernance <celia.fernandez.madrazo@cern.ch>
2025-04-08 02:22:26 -0700

    Add other interpretations to read limits

--- combineScripts/readAsymptoticLimits.py
+++ combineScripts/readAsymptoticLimits.py
@@ -3,7 +3,7 @@ import ROOT
 
 ROOT.gROOT.ProcessLine(".L cpp/helper.C+")
 
-useSignalMC = False
+useSignalMC = True
 
 if len(sys.argv)<3:
     print("Please, specify model and limit directory.")
@@ -21,27 +21,61 @@ else:
 
 fout = open("%s/limits_%s_%s.txt"%(outdir,model,year),"w")
 
-if useSignalMC:
-    #masses =  [0.5, 0.7, 1.5, 2.0, 2.5, 5.0, 6.0, 7.0, 8.0, 12.0, 14.0, 16.0, 20.0, 22.0, 24.0, 30.0, 34.0, 40.0, 44.0, 50.0] # Full set of masses
-    #masses =  [1.5, 2.0, 2.5, 5.0, 6.0, 7.0, 8.0, 14.0, 16.0, 20.0, 22.0, 24.0, 30.0, 34.0, 40.0, 44.0, 50.0]
-    masses =  [5.0, 8.0]
-    ctaus = [0.10, 0.16, 0.25, 0.40, 0.63, 1.00, 1.60, 2.50, 4.00, 6.30, 10.00, 16.00, 25.00, 40.00, 63.00, 100.00]
-else:
-    masses = []
-    ctaus = [1, 10, 100] # Lifetimes for the grid
-    with open('data/HZdZd_limitgrid.txt', 'r') as f:
-        lmasses = f.readlines()
-        for mass in lmasses:
-            m = float(mass)
-            masses.append(m)
-            print(m)
+if model=="HTo2ZdTo2mu2x":
+    if useSignalMC:
+        #masses =  [0.5, 0.7, 1.5, 2.0, 2.5, 5.0, 6.0, 7.0, 8.0, 12.0, 14.0, 16.0, 20.0, 22.0, 24.0, 30.0, 34.0, 40.0, 44.0, 50.0] # Full set of masses
+        masses =  [1.5, 2.0, 2.5, 5.0, 7.0, 8.0, 14.0, 16.0, 20.0, 22.0, 24.0, 30.0, 34.0, 40.0, 44.0, 50.0]
+        #masses =  [5.0, 8.0]
+        ctaus = [0.10, 0.16, 0.25, 0.40, 0.63, 1.00, 1.60, 2.50, 4.00, 6.30, 10.00, 16.00, 25.00, 40.00, 63.00, 100.00, 160.00, 250.00, 400.00, 630.00, 1000.00]
+    else:
+        masses = []
+        ctaus = [1, 10, 100, 1000] # Lifetimes for the grid
+        with open('data/sigmasses_HTo2ZdTo2mu2x_fine.txt', 'r') as f:
+            lmasses = f.readlines()
+            for mass in lmasses:
+                m = float(mass)
+                masses.append(m)
+                print(m)
+elif model=="BToPhi": 
+    if useSignalMC:
+        masses =  [0.25, 0.30, 0.40, 0.50, 0.60, 0.70, 0.90, 1.25, 1.50, 2.0, 2.85, 3.35, 4.00, 5.00] # Just full set of masses
+        ctaus = [0.1, 1, 10, 100]
+    else:
+        masses = []
+        ctaus = [0.1, 1, 10, 100] # Lifetimes for the grid
+        with open('data/BToPhi_limitgrid.txt', 'r') as f:
+            lmasses = f.readlines()
+            for mass in lmasses:
+                m = float(mass)
+                masses.append(m)
+                print(m)
+elif model=="ScenarioA": 
+    if useSignalMC:
+        masses =  [] 
+        #masses.append([5.0, 2.40])
+        masses.append([4.0, 1.33])
+        ctaus = [0.1, 1, 10, 100]
+    else:
+        print("No mass grid supported for this model")
+elif model=="ScenarioB1": 
+    if useSignalMC:
+        masses =  [] 
+        masses.append([5.0, 2.40])
+        #masses.append([4.0, 1.33])
+        ctaus = [0.1, 1, 10, 100]
+    else:
+        print("No mass grid supported for this model")
+
 
 f2bs = [0.0]
 if model == "nomodel":
     f2bs = [0.0,0.01,0.05,0.1,0.25,0.5,0.75,0.9,0.95,0.99,1.0]
     fout.write("# mass,f2b,obs,exp,exp-2s,exp-1s,exp+1s,exp+1s\n")
 else:
-    fout.write("# model,mass,ctau,obs,exp,exp-2s,exp-1s,exp+1s,exp+1s\n")
+    if "Scenario" not in model:
+        fout.write("# model,mass,ctau,obs,exp,exp-2s,exp-1s,exp+1s,exp+1s\n")
+    else:
+        fout.write("# model,mass1,mass2,ctau,obs,exp,exp-2s,exp-1s,exp+1s,exp+1s\n")
 
 for m in masses:
     for t in ctaus:
@@ -53,7 +87,10 @@ for m in masses:
             p1s = -1.0
             p2s = -1.0
             if model != "nomodel":
-                fname = "%s/lim_asymptotic_%s_m%.3f_ctau%.2f_%s.txt"%(limdir,model,m,t,year)
+                if "Scenario" not in model:
+                    fname = "%s/lim_asymptotic_%s_m%.3f_ctau%.2f_%s.txt"%(limdir,model,m,t,year)
+                else:
+                    fname = "%s/lim_asymptotic_%s_m%.3f_M%.3f_ctau%.2f_%s.txt"%(limdir,model,m[0],m[1],t,year)
                 print('Reading: ' + fname)
             else:
                 fname = "%s/lim_asymptotic_f2b%.0f_m%.0f.txt"%(limdir,100.0*f,m)
@@ -75,9 +112,13 @@ for m in masses:
                 elif "Expected 97.5" in l:
                     p2s = l.split()[len(l.split())-1]
             fin.close()
-            if model != "nomodel":
-                fout.write("%s,%.3f,%.2f,%s,%s,%s,%s,%s,%s\n"%(model,m,t,obs,exp,m2s,m1s,p1s,p2s))
-            else:
-                fout.write("%.0f,%.2f,%s,%s,%s,%s,%s,%s\n"%(m,f,obs,exp,m2s,m1s,p1s,p2s))
+            if float(obs) > -1 and float(exp) > -1:
+                if model != "nomodel":
+                    if "Scenario" not in model:
+                        fout.write("%s,%.3f,%.2f,%s,%s,%s,%s,%s,%s\n"%(model,m,t,obs,exp,m2s,m1s,p1s,p2s))
+                    else:
+                        fout.write("%s,%.3f,%.3f,%.2f,%s,%s,%s,%s,%s,%s\n"%(model,m[0],m[1],t,obs,exp,m2s,m1s,p1s,p2s))
+                else:
+                    fout.write("%.0f,%.2f,%s,%s,%s,%s,%s,%s\n"%(m,f,obs,exp,m2s,m1s,p1s,p2s))
 
 fout.close()
