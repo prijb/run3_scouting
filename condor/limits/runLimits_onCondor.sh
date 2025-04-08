b5062eb8b4a58f3a5c9eb58f1886d0d5100e8610
fernance <celia.fernandez.madrazo@cern.ch>
2025-04-08 02:25:56 -0700

    Update the jobs for limits

--- condor/limits/runLimits_onCondor.sh
+++ condor/limits/runLimits_onCondor.sh
@@ -19,7 +19,9 @@ if [ -z $1 ]; then usage; fi
 export SCOUTINGSNTINPUTDIRLIM=$1
 export SCOUTINGSNTOUTPUTDIRLIM=$2
 export PERIOD=$3
+export NAME=$4
 export HOMEDIR=$PWD
+export LABEL=$(basename $SCOUTINGSNTOUTPUTDIRLIM)
 
 echo "Creating dirs..."
 mkdir -p condor/limits/limits_logs
@@ -27,14 +29,19 @@ mkdir -p /ceph/cms/store/user/$USER/Run3ScoutingOutput/$SCOUTINGSNTOUTPUTDIRLIM
 
 echo "Preparing to create package..."
 sh condor/limits/create_package.sh $SCOUTINGSNTINPUTDIRLIM
+mv package.tar.gz package_${LABEL}.tar.gz
 
 ## Submission files to try extract limits on different subsets of signal samples
 #condor_submit condor/limits/runLimits_onCondor.sub
 #condor_submit condor/limits/runLimits_HTo2ZdTo2mu2x_onCondor.sub   # Mass grid for HTo2ZdTo2mu2x
+#condor_submit condor/limits/runLimits_HTo2ZdTo2mu2x_ultrafine_onCondor.sub   # Mass grid for HTo2ZdTo2mu2x (fine)
+#condor_submit condor/limits/runLimits_HTo2ZdTo2mu2x_toys_onCondor.sub   # Toys (reduced)
 #condor_submit condor/limits/runLimits_BToPhi_onCondor.sub          # Mass grid for BToPhi
 #condor_submit condor/limits/runLimits_BToPhi_SignalMC_onCondor.sub
-condor_submit condor/limits/runLimits_DQCD_SignalMC_onCondor.sub
+#condor_submit condor/limits/runLimits_ScenarioA_SignalMC_onCondor.sub
+#condor_submit condor/limits/runLimits_ScenarioB1_SignalMC_onCondor.sub
 #condor_submit condor/limits/runLimits_reweighting_limited_onCondor.sub
-#condor_submit condor/limits/runLimits_reweighting_onCondor.sub
+condor_submit condor/limits/runLimits_reweighting_onCondor.sub
+#condor_submit condor/limits/runLimits_reweighting_resubmit_onCondor.sub
 #condor_submit condor/limits/runLimits_reweightingValidation_onCondor.sub
 #condor_submit condor/limits/runLimits_limited_onCondor.sub
