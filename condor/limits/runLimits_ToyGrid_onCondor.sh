60452f13b156a67f7a3e8d27f6be2512a6a2075e
fernance <celia.fernandez.madrazo@cern.ch>
2025-05-01 08:47:35 -0700

    Add sub files to run on limits

---
+++ condor/limits/runLimits_ToyGrid_onCondor.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+export X509_USER_PROXY=$(voms-proxy-info -path)
+
+usage()
+{
+    echo "Usage:"
+    echo ""
+    echo "  sh condor/limits/runLimits_onCondor.sh [datacards] [output] [era/year] [type]"
+    echo ""
+    echo "The output_dir will be created in /ceph/cms/store/user/$USER/Run3ScoutingOutput/"
+    echo "Control the jobs to be run by editing the corresponding runLimits_onCondor.sub"
+    echo "Options:"
+    echo "-> [datacards]  e.g.  datacards_HTo2ZdTo2mu2x_Norm0.01_standard_Apr-15-2025_allEras"
+    echo "-> [output]     e.g.  limits_Apr-15-2025_HTo2ZdTo2mu2x_Norm0p01_vsMass_allEras"
+    echo "-> [era/year]   e.g.  allEras"
+    echo "-> [type]       e.g.  HTo2ZdTo2mu2x_mass_asymptotic"
+    echo ""
+    exit
+}
+
+if [ -z $1 ]; then usage; fi
+
+export SCOUTINGSNTINPUTDIRLIM=$1
+export SCOUTINGSNTOUTPUTDIRLIM=$2
+export PERIOD=$3
+export HOMEDIR=$PWD
+export LABEL=$(basename $SCOUTINGSNTOUTPUTDIRLIM)
+export TYPE=$4
+
+echo "Creating dirs..."
+mkdir -p condor/limits/limits_logs
+mkdir -p /ceph/cms/store/user/$USER/Run3ScoutingOutput/$SCOUTINGSNTOUTPUTDIRLIM
+
+echo "Preparing to create package..."
+#sh condor/limits/create_package.sh $SCOUTINGSNTINPUTDIRLIM
+#mv package.tar.gz package_${LABEL}.tar.gz
+
+## Submission files to try extract limits on different subsets of signal samples
+
+allmasses=(50.000)
+#allCTaus=(0.10 0.16 0.25 0.40 0.63 1.00 1.60 2.50 4.00 6.30 10.00 16.00 25.00 40.00 63.00 100.00 160.00 250.00 400.00 630.00 1000.00)
+allCTaus=(0.10)
+
+for m in ${allmasses[@]}
+do
+    for t in ${allCTaus[@]}
+    do
+        export MASS="${m}"
+        export CTAU="${t}"
+        condor_submit condor/limits/runLimits_HTo2ZdTo2mu2x_generalGrid_onCondor.sub
+    done
+done
